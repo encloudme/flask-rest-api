b26d9fa0273689b8f0af668d0cc084a23f611cce
encloudme <hkrkrishnakripa@gmail.com>
2021-10-23 18:50:39 +0530

    First commit, added REST API to access items, stores after user authentication.

---
+++ security.py
@@ -0,0 +1,17 @@
+# from werkzeug.security import safe_str_cmp
+from hmac import compare_digest
+from models.user import UserModel
+
+
+def authenticate(username, password):
+    # user = username_mapping.get(username, None)
+    user = UserModel.find_by_username(username)
+    if user and compare_digest(user.password, password):
+        # if user and safe_str_cmp(user.password, password):
+        # if user and user.password == password:
+        return user
+
+
+def identity(payload):
+    user_id = payload['identity']
+    return UserModel.find_by_id(user_id)
